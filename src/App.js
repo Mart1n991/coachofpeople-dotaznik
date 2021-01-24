1433b286d7ce3a2247ecd107daeb5f25154b621c
Mart1n991 <martinjavorsky8@gmail.com>
2021-01-24 12:55:34 +0100

    Vytvorenie globálneho pozadia

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { CssBaseline } from "@material-ui/core";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+
+//Vytvorenie témy z material UI
+const theme = createMuiTheme({
+  palette: {
+    background: {
+      paper: "#797b7d",
+      default: "#797b7d",
+    },
+  },
+});
+
 function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <h1>Dotazník</h1>
-    </div>
+    </ThemeProvider>
   );
 }
 
